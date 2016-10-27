98972718cf6b7d0c9db5ccdd3e19b8bbbd44162f
faranak <faranak.rzv@gmail.com>
2016-10-26 23:04:25 -0700

    torch pickup and light instantiation done

---
+++ Models/Minos/Assets/Scripts/Fading_Light.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fading_Light : MonoBehaviour
+{
+    public string state;
+    public Light pointLight;
+    public float fadingSpeed;
+    public float maxIntensity;
+    public Vector3 offset;
+
+	// Use this for initialization
+	void Start ()
+    {
+        pointLight.intensity = 1;
+        state = "lightUp";
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GameObject Player = GameObject.Find("FPSController");
+        //light must make what is infron't of the player, lighter
+        transform.position = Player.transform.position;
+        print("Light Intensity is : " + pointLight.intensity);
+        print("State is : " + state);
+
+        if(state == "lightUp")
+        {
+            lightUp();
+            
+        }
+        if(state == "Fading")
+        {
+            Fading();
+        }
+        
+
+    }
+    public void changeState(string stateName)
+    {
+        state = stateName;
+        print("current Light state is :" + stateName);
+    }
+
+    public void lightUp()
+    {
+        pointLight.intensity += fadingSpeed;
+        if (pointLight.intensity >= maxIntensity)
+        {
+            changeState("Fading");
+        }
+
+    }
+    public void Fading()
+    {
+        pointLight.intensity -= fadingSpeed;
+        if(pointLight.intensity == 0)
+        {
+            Destroy(this.gameObject);
+
+        }
+    }
+}
---
+++ Models/Minos/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Controller : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+    void OnTriggerEnter(Collider col)
+    {
+        Vector3 playerPosition = this.transform.position;
+        if(col.gameObject.tag == "Torch")
+        {
+            print("Name of the item I collide : " + col.gameObject);
+            //Pickup (destroiy the torch and then instantiate the light
+            Destroy(col.gameObject);
+            //Instantiate the light where the player is 
+            Light light = Instantiate(Resources.Load("point_light", typeof(Light)),
+                playerPosition, Quaternion.identity) as Light;
+        }
+    }
+}
