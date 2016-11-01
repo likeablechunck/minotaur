7644e4a4bcfb2c585365318aeda13f9e52a247bd
faranak <faranak.rzv@gmail.com>
2016-10-31 18:23:02 -0700

    work in progress for brazier's flame

---
+++ Models/Minos/Assets/Scripts/Light_Relocation.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Light_Relocation : MonoBehaviour {
+
+    public bool canPickUp;
+    public bool alreadyHasLight;
+
+    // Use this for initialization
+    void Start()
+    {
+        canPickUp = false;
+        alreadyHasLight = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!alreadyHasLight)
+        {
+            if (canPickUp && Input.GetKeyUp(KeyCode.Space))
+            {
+                PickUp();
+            }
+        }
+
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            canPickUp = true;
+
+        }
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            canPickUp = false;
+
+        }
+    }
+    void PickUp()
+    {
+
+    }
+}
