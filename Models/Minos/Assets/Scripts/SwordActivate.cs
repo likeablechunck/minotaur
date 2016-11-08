3d501343121093af4f49cfe4d068b7a3458ab38d
faranak <faranak.rzv@gmail.com>
2016-11-07 22:41:14 -0800

    more progress made; fmod implementation

---
+++ Models/Minos/Assets/Scripts/SwordActivate.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordActivate : MonoBehaviour {
+    public bool isClicked;
+    //private FMODUnity.StudioParameterTrigger swordEvent;
+    //public FMODUnity.StudioEventEmitter swordMusicEmitter;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //isClicked = false;
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        print("I collided into sword XD");
+        if(col.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+        
+        //swordEvent = this.GetComponent<FMODUnity.StudioParameterTrigger>();
+        //swordMusicEmitter.SetParameter("Intensity", 1f);
+
+
+    }
+}
