f9f1c957a4b2c03877b3f54e49745dcd9f92ac99
faranak <faranak.rzv@gmail.com>
2016-12-02 23:19:27 -0800

    both minotaur doors now open

---
+++ Models/Minos/Assets/Right_Minotaur_Door.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Right_Minotaur_Door : MonoBehaviour {
+  
+    public float doorLimit;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.Find("FPSController");
+        if (this.transform.position.x > doorLimit &&
+            player.GetComponent<Player_Controller>().shouldIOpenTheMinotaurDoor)
+        {
+            this.transform.Translate(-speed, 0, 0);
+        }
+    }
+}
