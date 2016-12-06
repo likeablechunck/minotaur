9b9b3d9534ee26f8995bea1f83fa7e7c9d53506c
faranak <faranak.rzv@gmail.com>
2016-12-06 11:27:05 -0800

    got rid of large file

---
+++ Models/Minos/Assets/Scripts/Credits.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour {
+
+	public void onClick()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
---
+++ Models/Minos/Assets/Scripts/Return_Button.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Return_Button : MonoBehaviour
+{
+
+	// Update is called once per frame
+	public void onClick()
+    {
+
+        SceneManager.LoadScene("Start");
+    }
+}
