2d50a4157ca5a6d6def243efea80e568fd2a8439
faranak <faranak.rzv@gmail.com>
2016-12-05 18:24:53 -0800

    Added the videos and how to implement them

---
+++ Models/Minos/Assets/Scripts/Opening_Video.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent (typeof (AudioSource))]
+
+public class Opening_Video : MonoBehaviour
+{
+    public MovieTexture movieText;
+	// Use this for initialization
+	void Start ()
+    {
+        this.GetComponent<Renderer>().material.mainTexture = movieText as MovieTexture;
+        movieText.Play();
+	
+	}
+	
+	
+}
