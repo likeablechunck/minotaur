dbe6e14ef148455a3caa88c65811cbd06fb89134
faranak <faranak.rzv@gmail.com>
2016-12-08 13:16:01 -0800

    replaced old videos with the new ones

---
+++ Models/Minos/Assets/Scripts/Reset_Braziers.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reset_Braziers : MonoBehaviour
+{
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GameObject player = GameObject.Find("FirstPersonCharacter");
+        if(player.GetComponent<Old_Lady_Controller>().gameTimer <=0)
+        {
+            Reset();
+        }
+	
+	}
+
+    public void Reset()
+    {
+
+    }
+}
